c38a96e98d08c0cd07923c6f58ec6582b32be58d
Farid Rener <proteusvacuum@gmail.com>
2015-03-19 16:51:10 -0400

    poop

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -42,7 +42,7 @@ need_push () {
   then
     echo " "
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    echo " with %{$fg_bold[magenta]%}unpushed 💩%{$reset_color%} "
   fi
 }
 
