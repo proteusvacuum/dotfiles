f95c9ac1c35991fe95059749d94ca16aaa06a612
Farid Rener <proteusvacuum@gmail.com>
2015-05-12 00:16:03 +0200

    add more python things and tmux fun

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,3 @@
 alias djm='python manage.py'
-alias djserve='python manage.py runserver_plus'
-alias djshell='python manage.py shell_plus'
+alias djserve='python manage.py runserver'
+alias djshell='python manage.py shell'
