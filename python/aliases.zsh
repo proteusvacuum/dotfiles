f40cb773066da5e986f9b625b4199be60f304a99
Farid Rener <proteusvacuum@gmail.com>
2016-02-23 19:55:40 +0530

    Add django test alias

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,5 +1,6 @@
 alias djm='python manage.py'
 alias djserve='python manage.py runserver'
 alias djshell='python manage.py shell_plus'
+alias djt = 'python manage.py test'
 
 alias whq='workon cchq; cd ~/dev/dimagi/commcare-hq'
