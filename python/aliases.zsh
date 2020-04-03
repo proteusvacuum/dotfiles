60eb44e3c9e502f29290bb0e9ebc48b820a4786c
Farid Rener <proteusvacuum@gmail.com>
2020-04-02 21:53:01 -0400

    April

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -3,5 +3,5 @@ alias djserve='python manage.py runserver_plus 0.0.0.0:8000'
 alias djshell='python manage.py shell_plus'
 alias djt='REUSE_DB=1 python manage.py test'
 
-alias whq='workon cchq; cd ~/dev/dimagi/commcare-hq'
+alias whq='workon cchq-py3; cd ~/dev/dimagi/commcare-hq'
 alias wca='workon ccansible; cd ~/dev/dimagi/commcarehq-ansible'
