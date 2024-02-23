a247142360208a4fcf0eff94c895262ce648195b
Farid Rener <proteusvacuum@gmail.com>
2024-02-23 14:42:16 -0500

    2024-02

--- java/path.zsh
+++ java/path.zsh
@@ -1,5 +1,5 @@
-export PATH="$HOME/dev/java/jdk1.8.0_101/bin:$PATH"
-export JAVA_HOME=$HOME/dev/java/jdk1.8.0_101
+# export PATH="$HOME/dev/java/jdk1.8.0_101/bin:$PATH"
+# export JAVA_HOME=$HOME/dev/java/jdk1.8.0_101
 # export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
 export CATALINA_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=1024m"
--- python/path.zsh
+++ python/path.zsh
@@ -1,7 +1,8 @@
-export DJANGO_PROJECT_ROOT=$PROJECTS/dimagi/commcare-hq
-export PYTHONPATH=$DJANGO_PROJECT_ROOT:$DJANGO_PROJECT_ROOT/corehq/ex-submodules
+# export DJANGO_PROJECT_ROOT=$PROJECTS/dimagi/commcare-hq
+# export PYTHONPATH=$DJANGO_PROJECT_ROOT:$DJANGO_PROJECT_ROOT/corehq/ex-submodules
 
-for MODULE in `ls $DJANGO_PROJECT_ROOT/submodules`
-do
-    export PYTHONPATH=$PYTHONPATH:$DJANGO_PROJECT_ROOT/submodules/$MODULE
-done
+# for MODULE in `ls $DJANGO_PROJECT_ROOT/submodules`
+# do
+#     export PYTHONPATH=$PYTHONPATH:$DJANGO_PROJECT_ROOT/submodules/$MODULE
+# done
+export PATH="$HOME/.virtualenvs/system/bin:$PATH"
