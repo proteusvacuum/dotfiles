a631ca8bd268c1666f11bd308fc1a2a0ed41e6f4
Farid Rener <proteusvacuum@gmail.com>
2025-12-10 21:57:48 -0500

    Gate linux aliases behind flag

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,10 @@
 alias reload!='. ~/.zshrc'
-alias open='xdg-open'
 
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xclip -selection clipboard -o'
+# Linux-specific aliases
+if [[ "$OSTYPE" == linux* ]]; then
+  alias open='xdg-open'
+  alias pbcopy='xsel --clipboard --input'
+  alias pbpaste='xclip -selection clipboard -o'
+fi
 
 alias grep='grep --color'
