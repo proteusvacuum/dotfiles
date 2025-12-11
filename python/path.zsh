c273372d85357bbcb4bd8440c699097819f1b5ca
Farid Rener <proteusvacuum@gmail.com>
2025-12-10 21:47:39 -0500

    More macos

--- python/path.zsh
+++ python/path.zsh
@@ -1,9 +1,2 @@
-# export DJANGO_PROJECT_ROOT=$PROJECTS/dimagi/commcare-hq
-# export PYTHONPATH=$DJANGO_PROJECT_ROOT:$DJANGO_PROJECT_ROOT/corehq/ex-submodules
-
-# for MODULE in `ls $DJANGO_PROJECT_ROOT/submodules`
-# do
-#     export PYTHONPATH=$PYTHONPATH:$DJANGO_PROJECT_ROOT/submodules/$MODULE
-# done
-# export PATH="$HOME/.virtualenvs/system/bin:$PATH"
-
+export UV_KEYRING_PROVIDER=subprocess
+export UV_INDEX_OVERSTORY_USERNAME=oauth2accesstoken
